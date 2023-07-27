780ef033ebcef8441cfbad23a78259e9450825b5
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-07-27 11:07:05 -0700

    Add more partials styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const apiKey = process.env.API_KEY;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Express Space App' });
+  console.log("apiKey: ", apiKey)
 });
 
 module.exports = router;
