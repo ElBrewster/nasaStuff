3fdd7bc6b750d139d09c11587bc675c335f3dd92
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-07-27 15:39:41 -0700

    Add dayjs and api call functionality to display photos from current date

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,46 @@ var express = require('express');
 var router = express.Router();
 const request = require("request");
 
+const dayjs = require("dayjs");
+var utc = require('dayjs/plugin/utc');
+var timezone = require('dayjs/plugin/timezone');
+// dayjs().format(); 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+let nowObject = dayjs();
+let nowISO8601String = nowObject.$d;
+
+let nowYear = nowObject.$y;
+let nowMonth = nowObject.$M;
+let nowDay = nowObject.$D;
+let today = `${nowYear}-${nowMonth}-${nowDay}`;
+
 const apiKey = process.env.API_KEY;
+
 // * Mars Rover Photos
 const apiUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity";
 const apiParamSol = `/photos?sol=1000&api_key=${apiKey}`;
 const apiParamCamera = `/photos?sol=1000&camera=fhaz&api_key=${apiKey}`;
 const apiPage2 = `/photos?sol=1000&page=2&api_key=${apiKey}`;
-const apiParamEarthDate = `/photos?earth_date=2015-6-3&api_key=${apiKey}`;
+const apiParamEarthDate = `/photos?earth_date=${today}&api_key=${apiKey}`;
 const apiParamLatestPhotos = `/latest_photos?api_key=${apiKey}`;
 //https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY
 const currentPhoto = `${apiUrl}${apiParamSol}`;
+const picturesToday = `${apiUrl}${apiParamEarthDate}`;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  request.get(currentPhoto, (error, response, roverData) => {
-    // console.log("===================The Error===================");
-    // console.log(error);
-    // console.log("===================The Response===================");
-    // console.log(response);
-    // console.log(roverData);
+  // request.get(currentPhoto, (error, response, roverData) => {
+  //   const parsedData = JSON.parse(roverData);
+  //   res.render("index", {
+  //     parsedData: parsedData.photos
+  //   })
+  // });
+  request.get(picturesToday, (error, response, roverData) => {
     const parsedData = JSON.parse(roverData);
-    // console.log(parsedData.photos[0]);
-    // res.json(parsedData);
     res.render("index", {
       parsedData: parsedData.photos
     })
   })
-  // res.render('index', { title: 'My Express Space App' });
-  // console.log("apiKey: ", apiKey)
 });
 
 module.exports = router;
