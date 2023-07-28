94c7866b5d7f6164e95c89bdbb2f0480b5efd52a
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-07-28 13:01:35 -0700

    Add basic nav bar styling

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ let today = `${nowYear}-${nowMonth}-${nowDay}`;
 
 const apiKey = process.env.API_KEY;
 
-const roversList = ["curiosity", "opportunity", "perseverance", "spirit"];
+const roversList = ["CURIOSITY", "OPPORTUNITY", "PERSEVERANCE", "SPIRIT"];
 // * Mars Rover Photos
 const apiRoversUrlDefault = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity";
 const apiRoversUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers";
