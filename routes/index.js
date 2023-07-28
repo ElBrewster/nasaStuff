eb71b1dad27c2d90bf135f78f0579b3cbb806c37
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-07-28 10:42:48 -0700

    Add href to links to individual photos across all rover views

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,6 @@ var timezone = require('dayjs/plugin/timezone');
 dayjs.extend(utc);
 dayjs.extend(timezone);
 let nowObject = dayjs();
-let nowISO8601String = nowObject.$d;
-
 let nowYear = nowObject.$y;
 let nowMonth = nowObject.$M;
 let nowDay = nowObject.$D;
@@ -19,7 +17,9 @@ let today = `${nowYear}-${nowMonth}-${nowDay}`;
 const apiKey = process.env.API_KEY;
 
 // * Mars Rover Photos
-const apiUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity";
+const apiRoversUrlDefault = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity";
+const apiRoversUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers";
+//need rover name after this url piece ^
 const apiParamSol = `/photos?sol=1000&api_key=${apiKey}`;
 const apiParamCamera = `/photos?sol=1000&camera=fhaz&api_key=${apiKey}`;
 const apiPage2 = `/photos?sol=1000&page=2&api_key=${apiKey}`;
@@ -28,22 +28,29 @@ const apiParamEarthDate = `/photos?earth_date=${today}&page=1&api_key=${apiKey}`
 
 const apiParamLatestPhotos = `/latest_photos?api_key=${apiKey}`;
 //https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY
-const currentPhoto = `${apiUrl}${apiParamSol}`;
-const picturesToday = `${apiUrl}${apiParamEarthDate}`;
+const currentPhoto = `${apiRoversUrlDefault}${apiParamSol}`;
+const picturesToday = `${apiRoversUrl}/roverName${apiParamEarthDate}`;
+// template ^
+router.use((req, res, next) => {
+  res.locals.apiRoversUrl = apiRoversUrl;
+  res.locals.apiParamEarthDate = apiParamEarthDate;
+  next();
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // request.get(currentPhoto, (error, response, roverData) => {
-  //   const parsedData = JSON.parse(roverData);
-  //   res.render("index", {
-  //     parsedData: parsedData.photos
-  //   })
-  // });
-  request.get(picturesToday, (error, response, roverData) => {
+  request.get(currentPhoto, (error, response, roverData) => {
     const parsedData = JSON.parse(roverData);
     res.render("index", {
       parsedData: parsedData.photos
     })
-  })
+  });
+  // request.get(picturesToday, (error, response, roverData) => {
+  //   const parsedData = JSON.parse(roverData);
+  //   res.render("index", {
+  //     parsedData: parsedData.photos
+  //   })
+  // })
 });
 
 module.exports = router;
---
+++ routes/perseverance.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+const request = require("request");
+
+const dayjs = require("dayjs");
+var utc = require('dayjs/plugin/utc');
+var timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+let nowObject = dayjs();
+
+let nowYear = nowObject.$y;
+let nowMonth = nowObject.$M;
+let nowDay = nowObject.$D;
+let today = `${nowYear}-${nowMonth}-${nowDay}`;
+
+const apiKey = process.env.API_KEY;
+
+// * Mars Rover Photos
+const apiUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance";
+const apiParamEarthDate = `/photos?earth_date=${today}&api_key=${apiKey}`;
+const apiParamLatestPhotos = `/latest_photos?api_key=${apiKey}`;
+const picturesToday = `${apiUrl}${apiParamEarthDate}`;
+const mostRecentPhotos = `${apiUrl}${apiParamLatestPhotos}`;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // request.get(mostRecentPhotos, (error, response, roverData) => {
+  //   const parsedData = JSON.parse(roverData);
+  //   res.render("index", {
+  //     parsedData: parsedData.photos
+  //   })
+  // });
+  request.get(picturesToday, (error, response, roverData) => {
+    const parsedData = JSON.parse(roverData);
+    res.render("index", {
+      parsedData: parsedData.photos
+    })
+  })
+});
+
+module.exports = router;
