e2c1fb5de9e8ace86cebe4365dd90f22e7ee24c3
elbie <113723085+ElBrewster@users.noreply.github.com>
2023-08-01 10:28:23 -0700

    Revert "Feature/login"

--- routes/index.js
+++ routes/index.js
@@ -12,21 +12,17 @@ let nowYear = nowObject.$y;
 let nowMonth = nowObject.$M;
 let nowDay = nowObject.$D;
 let today = `${nowYear}-${nowMonth}-${nowDay}`;
-// TODO: Add time zone options so my friend in Germany can pick their local time
 
 const apiKey = process.env.API_KEY;
 
 const roversList = ["curiosity", "opportunity", "perseverance", "spirit"];
 
-// * Mars Rover Photos URL bits
+// * Mars Rover Photos
 const apiRoversUrlDefault = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity";
 const apiRoversBaseUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers";
 const apiParamEarthDate = `/photos?earth_date=${today}&page=1&api_key=${apiKey}`;
 const apiParamLatestPhotos = `/latest_photos?api_key=${apiKey}`;
 const currentPhoto = `${apiRoversUrlDefault}${apiParamLatestPhotos}`;
-const manifestEndpoint = `${apiRoversBaseUrl}/${roversList[0]}?&api_key=${apiKey}`;
-// TODO: Add rover data to home page instead of photo sample with `apiRoversUrlDefault` endpoint
-// TODO: Add toggle between Sol date and Earth date search field
 
 router.use((req, res, next) => {
   res.locals.roversList = roversList;
@@ -53,6 +49,7 @@ router.get("/curiosity", (req, res, next) => {
     })
   })
 });
+
 
 router.get("/opportunity", (req, res, next) => {
   const curiosityPicsToday = `${apiRoversBaseUrl}/opportunity${apiParamLatestPhotos}`;
@@ -85,20 +82,4 @@ router.get("/spirit", (req, res, next) => {
   })
 });
 
-router.post("/search", (req, res, next) => {
-  const searchTerm = req.body.roverSelection;
-  const searchDate = req.body.solDate;
-  const searchDateUrl = `${apiRoversBaseUrl}/${searchTerm}/photos?sol=${searchDate}&api_key=${apiKey}`;
-  request.get(searchDateUrl, (error, response, oneSolData) => {
-    const parsedData = JSON.parse(oneSolData);
-    res.render("search", {
-      parsedData: parsedData.photos
-    })
-  })
-});
-// TODO: Need error handling for sols that have no photos
-// TODO: Need search bar limitations so that only 1-4 digits can be entered, and perhaps a legend to indicate what dates are available
-
 module.exports = router;
-
-
