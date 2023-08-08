08950c6f5219759097d917b26564ef150ce023c2
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-08-08 08:00:13 -0700

    Remove vercel json file and express app comments in app js file

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(helmet({
   })
 }));
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'ejs');
 
@@ -37,18 +36,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
